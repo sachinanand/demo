39446fe8514ceaa28f5e142ae7d78a590676c1bc
Sachin <Sachin Anand>
2016-02-09 10:26:00 +0530

    Domain classes now persisting to database

--- src/main/groovy/com/ttn/domain/User.groovy
+++ src/main/groovy/com/ttn/domain/User.groovy
@@ -1,18 +1,27 @@
 package com.ttn.domain
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+import org.aspectj.apache.bcel.generic.TABLESWITCH
 import org.hibernate.validator.constraints.Email
-import org.springframework.data.annotation.Id
 
 import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.Table
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
 /**
  * Created by sachin on 08/02/16.
  */
+@Entity
+@Table(name="`user`")
 public class User extends AbstractAuditingEntity implements Serializable {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String id;
 
     @NotNull
@@ -20,6 +29,7 @@ public class User extends AbstractAuditingEntity implements Serializable {
     @Column(unique = true)
     @Size(max = 100)
     @Email
+    @JsonIgnore
     private String email;
 
     String getId() {
